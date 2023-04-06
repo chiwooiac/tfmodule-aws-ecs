eecd3031df7f7dcd86062b7f5c9eece753ddbec9
symplesims <symplesims@gmail.com>
2023-04-06 09:57:04 +0900

    aws_ecs_cluster_capacity_providers 리소스 구성 보완

--- main.tf
+++ main.tf
@@ -6,19 +6,6 @@ resource "aws_ecs_cluster" "this" {
   count = var.create_ecs ? 1 : 0
 
   name = local.cluster_name
-  capacity_providers = var.capacity_providers
-
-  dynamic "default_capacity_provider_strategy" {
-    for_each = var.default_capacity_provider_strategy
-    iterator = strategy
-
-    content {
-      capacity_provider = strategy.value["capacity_provider"]
-      weight            = lookup(strategy.value, "weight", null)
-      base              = lookup(strategy.value, "base", null)
-    }
-  }
-
   setting {
     name  = "containerInsights"
     value = var.container_insights ? "enabled" : "disabled"
@@ -27,5 +14,21 @@ resource "aws_ecs_cluster" "this" {
   tags = merge(var.context.tags,
     { Name = local.cluster_name }
   )
+}
 
+resource "aws_ecs_cluster_capacity_providers" "this" {
+  cluster_name       = aws_ecs_cluster.this.name
+  capacity_providers = var.capacity_providers
+
+  dynamic "default_capacity_provider_strategy" {
+    for_each = var.default_capacity_provider_strategy
+    iterator = strategy
+    content {
+      capacity_provider = strategy.value["capacity_provider"]
+      weight            = lookup(strategy.value, "weight", null)
+      base              = lookup(strategy.value, "base", null)
+    }
+  }
+
+  depends_on         = [aws_ecs_cluster.this]
 }
